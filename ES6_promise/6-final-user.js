b8804bf0c53c66c3592a2e27e4ed616e39ea9880
layan <layansaleem@gmail.com>
2026-04-04 18:45:51 +0000

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,17 +1,14 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  // Call both functions to get their promises
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-  
-  // Use Promise.allSettled to wait for both promises to settle
-  const results = await Promise.allSettled([signUpPromise, uploadPromise]);
-  
-  // Map the results to the required format
-  return results.map(result => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason
-  }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ]).then(results => {
+    return results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason
+    }));
+  });
 }
