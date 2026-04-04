fefff45fbbd08dcdfa0a21766fff8b9e0fe0be10
layan <layansaleem@gmail.com>
2026-04-04 18:48:44 +0000

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,11 +4,13 @@ import uploadPhoto from './5-photo-reject.js';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ]).then(results => {
-    return results.map(result => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason
-    }));
-  });
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((res) => {
+      if (res.status === 'fulfilled') {
+        return { status: res.status, value: res.value };
+      }
+      return { status: res.status, value: res.reason.toString() };
+    })
+  );
 }
