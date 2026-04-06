d87c7be1c8f390f4e7d19a06a21bc6b18559b6a4
layan <layansaleem@gmail.com>
2026-04-06 11:37:44 +0000

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
-const displayMessage = require('./0-console');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-displayMessage('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (data) => process.stdout.write(`Your name is: ${data}`));
-
-process.stdin.on('end', () => displayMessage('This important software is now closing'));
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
