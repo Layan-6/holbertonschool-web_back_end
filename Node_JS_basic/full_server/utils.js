c4010588864a72964c11cf6441d4a12974e95085
layan <layansaleem@gmail.com>
2026-05-11 09:09:41 +0000

    8

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+const readDatabase = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n').filter(line => line.trim() !== '');
+      const students = {};
+      
+      // Skip header line
+      for (let i = 1; i < lines.length; i++) {
+        const [firstname, lastname, age, field] = lines[i].split(',');
+        
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      }
+
+      resolve(students);
+    });
+  });
+};
+
+export default readDatabase;
